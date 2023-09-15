7d93b7d85820abf31bf3e90f56f032f6681931ae
Eduardo Schmitt <zeduhhh@gmail.com>
2023-09-15 09:17:04 -0300

    att

--- routes/articles.js
+++ routes/articles.js
@@ -17,15 +17,18 @@ function updateArticle(articleId, newData) {
     if (index !== -1) {
         const updatedArticle = { ...articlesData[index] };
 
-        if (newData.kb_title) {
-            updatedArticle.kb_title = newData.kb_title;
-        }
-        if (newData.kb_image) {
-            updatedArticle.kb_image = newData.kb_image;
-        }
-        if (newData.kb_body) {
-            updatedArticle.kb_body = newData.kb_body;
-        }
+        updatedArticle.kb_title = newData.kb_title || updatedArticle.kb_title;
+        updatedArticle.kb_description = newData.kb_description || updatedArticle.kb_description;
+        updatedArticle.kb_body = newData.kb_body || updatedArticle.kb_body;
+        updatedArticle.kb_permalink = newData.kb_permalink || updatedArticle.kb_permalink;
+        updatedArticle.kb_keywords = newData.kb_keywords || updatedArticle.kb_keywords;
+        updatedArticle.kb_liked_count = newData.kb_liked_count !== undefined ? newData.kb_liked_count : updatedArticle.kb_liked_count;
+        updatedArticle.kb_published = newData.kb_published !== undefined ? newData.kb_published : updatedArticle.kb_published;
+        updatedArticle.kb_suggestion = newData.kb_suggestion !== undefined ? newData.kb_suggestion : updatedArticle.kb_suggestion;
+        updatedArticle.kb_featured = newData.kb_featured !== undefined ? newData.kb_featured : updatedArticle.kb_featured;
+        updatedArticle.kb_author_email = newData.kb_author_email || updatedArticle.kb_author_email;
+        updatedArticle.kb_published_date = newData.kb_published_date || updatedArticle.kb_published_date;
+        updatedArticle.kb_image = newData.kb_image || updatedArticle.kb_image;
 
         articlesData[index] = updatedArticle;
 
@@ -37,6 +40,47 @@ function updateArticle(articleId, newData) {
     return null;
 }
 
+function createArticle(newData) {
+    const filePath = path.join(__dirname, '../data/articles.json');
+    const jsonData = fs.readFileSync(filePath, 'utf8');
+    const articlesData = JSON.parse(jsonData);
+
+    const newArticleId = generateNewArticleId();
+
+    const newArticle = {
+        kb_id: newArticleId,
+        kb_title: newData.kb_title,
+        kb_description: newData.kb_description,
+        kb_body: newData.kb_body,
+        kb_permalink: newData.kb_permalink,
+        kb_keywords: newData.kb_keywords,
+        kb_liked_count: newData.kb_liked_count,
+        kb_published: newData.kb_published,
+        kb_suggestion: newData.kb_suggestion,
+        kb_featured: newData.kb_featured,
+        kb_author_email: newData.kb_author_email,
+        kb_published_date: newData.kb_published_date,
+        kb_image: newData.kb_image,
+    };
+
+    articlesData.push(newArticle);
+
+    fs.writeFileSync(filePath, JSON.stringify(articlesData, null, 2));
+
+    return newArticle;
+}
+
+function generateNewArticleId() {
+    const characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    let id = '';
+  
+    for (let i = 0; i < 10; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      id += characters[randomIndex];
+    }
+  
+    return id;
+}
 
 router.get("/edit", (req, res) => {
     res.render("articles_edit");
@@ -53,12 +97,28 @@ router.get('/edit/:id', (req, res) => {
     }
 });
 
+
+// Criar artigo
+
+router.get("/criar", (req, res) => {
+    res.render("articles_create");
+});
+
 router.post('/salvar/:id', (req, res) => {
     const articleId = req.params.id;
     const newData = {
-        kb_title: req.body.title,
-        kb_image: req.body.image,
-        kb_body: req.body.body,
+        kb_title: req.body.kb_title,
+        kb_description: req.body.kb_description,
+        kb_body: req.body.kb_body,
+        kb_permalink: req.body.kb_permalink,
+        kb_keywords: req.body.kb_keywords,
+        kb_liked_count: parseInt(req.body.kb_liked_count), // Converte a string para número inteiro
+        kb_published: req.body.kb_published === 'true', // Converte a string 'true' para booleano true
+        kb_suggestion: req.body.kb_suggestion === 'true', // Converte a string 'true' para booleano true
+        kb_featured: req.body.kb_featured === 'true', // Converte a string 'true' para booleano true
+        kb_author_email: req.body.kb_author_email,
+        kb_published_date: req.body.kb_published_date,
+        kb_image: req.body.kb_image,
     };
 
     const updatedArticle = updateArticle(articleId, newData);
@@ -69,6 +129,37 @@ router.post('/salvar/:id', (req, res) => {
         return res.status(404).json({ message: 'Artigo não encontrado' });
     }
 });
+
+router.post('/criar', (req, res) => {
+    const newData = {
+        kb_title: req.body.kb_title,
+        kb_description: req.body.kb_description,
+        kb_image: req.body.kb_image,
+        kb_body: req.body.kb_body,
+        kb_permalink: req.body.kb_permalink,
+        kb_keywords: req.body.kb_keywords,
+        kb_liked_count: parseInt(req.body.kb_liked_count),
+        kb_published: req.body.kb_published === 'true',
+        kb_suggestion: req.body.kb_suggestion === 'true',
+        kb_featured: req.body.kb_featured === 'true',
+        kb_author_email: req.body.kb_author_email,
+        kb_published_date: req.body.kb_published_date,
+    };
+
+    const newArticle = createArticle(newData);
+
+    if (newArticle) {
+        return res.status(201).json({ message: 'Artigo criado com sucesso', article: newArticle });
+    } else {
+        return res.status(400).json({ message: 'Erro ao criar o artigo' });
+    }
+});
+
+// Editar Usuário
+router.get("/editar/user", (req, res) => {
+    res.render("users_edit");
+});
+
 
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,70 @@ const express = require("express");
 const router = express.Router();
 const fs = require("fs");
 const userData = require("../data/users.json");
+const path = require('path');
 const articlesData = require('../data/articles.json');
 
 router.use(express.json());
+
+function updateUser(userId, newData) {
+  const filePath = path.join(__dirname, '../data/users.json');
+  const jsonData = fs.readFileSync(filePath, 'utf8');
+  const usersData = JSON.parse(jsonData);
+  const index = usersData.findIndex((user) => user.author_id === userId);
+
+  if (index !== -1) {
+      const updatedUser = { ...usersData[index] };
+
+      updatedUser.author_name = newData.author_name || updatedUser.author_name;
+      updatedUser.author_email = newData.author_email || updatedUser.author_email;
+      updatedUser.author_user = newData.author_user || updatedUser.author_user;
+      updatedUser.author_pwd = newData.author_pwd || updatedUser.author_pwd;
+      updatedUser.author_level = newData.author_level || updatedUser.author_level;
+      updatedUser.author_status = newData.author_status || updatedUser.author_status;
+
+      usersData[index] = updatedUser;
+
+      fs.writeFileSync(filePath, JSON.stringify(usersData, null, 2));
+
+      return updatedUser;
+  }
+
+  return null;
+}
+
+function createUser(newData) {
+  const filePath = path.join(__dirname, '../data/users.json');
+  const jsonData = fs.readFileSync(filePath, 'utf8');
+  const usersData = JSON.parse(jsonData);
+
+  const newUser = {
+      author_id: generateNewUserId(),
+      author_name: newData.author_name,
+      author_email: newData.author_email,
+      author_user: newData.author_user,
+      author_pwd: newData.author_pwd,
+      author_level: newData.author_level,
+      author_status: newData.author_status,
+  };
+
+  usersData.push(newUser);
+
+  fs.writeFileSync(filePath, JSON.stringify(usersData, null, 2));
+
+  return newUser;
+}
+
+function generateNewUserId() {
+  const characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  let id = '';
+
+  for (let i = 0; i < 40; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    id += characters[randomIndex];
+  }
+
+  return id;
+}
 
 router.get("/", (req, res) => {
   res.render("login");
@@ -26,6 +87,68 @@ router.post("/admin", (req, res) => {
   } else {
     //return res.status(401).json({ message: "Credenciais inválidas." });
     return res.render('index', { articles: articlesData });
+  }
+});
+
+router.get("/usuarios", (req, res) => {
+  return res.render('users', { users: userData });
+});
+
+// Editar Usuário
+
+router.get('/edit/:id', (req, res) => {
+  const userId = req.params.id;
+  const user = userData.find(user => user.author_id === userId);
+
+  if (!user) {
+      res.render('error', { message: 'Usuário não encontrado' });
+  } else {
+    res.render('users_edit', { user });
+  }
+});
+
+router.post('/salvar/:id', (req, res) => {
+  const userId = req.params.id;
+  const newData = {
+      author_name: req.body.author_name,
+      author_email: req.body.author_email,
+      author_user: req.body.author_user,
+      author_pwd: req.body.author_pwd,
+      author_level: req.body.author_level,
+      author_status: req.body.author_status,
+  };
+
+  const updatedUser = updateUser(userId, newData);
+
+  if (updatedUser) {
+      return res.status(200).json({ message: 'Usuário atualizado com sucesso', user: updatedUser });
+  } else {
+      return res.status(404).json({ message: 'Usuário não encontrado' });
+  }
+});
+
+// Criar Usuário
+
+router.get('/criar', (req, res) => {
+  res.render('users_create');
+});
+
+router.post('/criar', (req, res) => {
+  const newData = {
+      author_name: req.body.author_name,
+      author_email: req.body.author_email,
+      author_user: req.body.author_user,
+      author_pwd: req.body.author_pwd,
+      author_level: req.body.author_level,
+      author_status: req.body.author_status,
+  };
+
+  const newUser = createUser(newData);
+
+  if (newUser) {
+      return res.status(201).json({ message: 'Usuário criado com sucesso', user: newUser });
+  } else {
+      return res.status(400).json({ message: 'Erro ao criar o usuário' });
   }
 });
 
